a5d5c7c95e82733b57617bb4704698204f68fa46
SnayeulTedy <161803875+SnayeulTedy@users.noreply.github.com>
2024-05-10 12:40:14 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TRADUCTEUR CODE MORSE";
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTranslate.Location = new System.Drawing.Point(153, 147);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(150, 39);
+            this.btnTranslate.TabIndex = 2;
+            this.btnTranslate.Text = "Traduire";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(47, 87);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(400, 30);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtOutput.Location = new System.Drawing.Point(47, 203);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(400, 151);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 383);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 81);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 505);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Traducteur Code Morse";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOS
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<char, string> morseAlphabet;
+        public Form1()
+        {
+            InitializeComponent();
+            InitialiseMorseAlphabet();
+        }
+
+        private void InitialiseMorseAlphabet()
+        {
+            morseAlphabet = new Dictionary<char, string>()
+            {
+               {'a', ".-"}, {'b', "-..."}, {'c', "-.-."}, {'d', "-.."}, {'e', "."}, {'f', "..-."},
+                {'g', "--."}, {'h', "...."}, {'i', ".."}, {'j', ".---"}, {'k', "-.-"}, {'l', ".-.."},
+                {'m', "--"}, {'n', "-."}, {'o', "---"}, {'p', ".--."}, {'q', "--.-"}, {'r', ".-."},
+                {'s', "..."}, {'t', "-"}, {'u', "..-"}, {'v', "...-"}, {'w', ".--"}, {'x', "-..-"},
+                {'y', "-.--"}, {'z', "--.."}, {'0', "-----"}, {'1', ".----"}, {'2', "..---"},
+                {'3', "...--"}, {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."},
+                {'8', "---.."}, {'9', "----."}
+            };
+        }
+
+        private string TranslationToMorse(string input)
+        {
+            StringBuilder morseCode = new StringBuilder();
+            foreach(char character in input.ToLower())
+            {
+                if(morseAlphabet.ContainsKey(character))
+                {
+                    morseCode.Append(morseAlphabet[character] + " ");
+                }
+                else if(character == ' ')
+                {
+                    morseCode.Append("/ ");
+                }
+                else
+                {
+                    morseCode.Append(character + " ");
+                }
+            }
+            return morseCode.ToString();
+        }
+
+        private void PlayMorseAudio(string morseCode)
+        {
+            System.Threading.Thread audioThread = new System.Threading.Thread(() =>
+            {
+                foreach (char symbol in morseCode)
+                {
+                    if (symbol == '.')
+                    {
+                        PlaySound("dot.wav");
+                    }
+                    else if (symbol == '_')
+                    {
+                        PlaySound("dash.wav");
+                    }
+                    else if (symbol == ' ')
+                    {
+                        System.Threading.Thread.Sleep(100); // la pause entre les characteres
+                    }
+                }
+            });
+            audioThread.Start();
+        }
+
+        private void PlaySound(string soundFile)
+        {
+            try
+            {
+                string filePath = Application.StartupPath + "\\" + soundFile;
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(filePath);
+                player.Play();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la lecture du son :" + ex.Message);
+            }
+        }
+
+        private void DisplayMorseVisual(string morseCode)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                g.Clear(Color.White);
+                int x = 10;
+                int y = 20;
+                int symbolWidth = 20;
+
+                foreach(char symbol in morseCode)
+                {
+                    if(symbol == '.')
+                    {
+                        g.FillEllipse(Brushes.Black, x, y, symbolWidth, symbolWidth);
+                    }
+                    else if(symbol == '_')
+                    {
+                        g.FillEllipse(Brushes.Black, x, y, symbolWidth = 3, symbolWidth);
+                    }
+
+                    x += symbolWidth * 4;
+                }
+            }
+
+            pictureBox1.Invoke((MethodInvoker)delegate
+            {
+                pictureBox1.Image?.Dispose();
+                pictureBox1.Image = image;
+            });
+        }
+
+
+
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            string userInput = txtInput.Text;
+            string morseTranslation = TranslationToMorse(userInput);
+            txtOutput.Text = morseTranslation;
+
+            PlayMorseAudio(morseTranslation);
+
+            DisplayMorseVisual(morseTranslation);
+        }
+    }
+}
